b5be7ae4cafcb7985b073f50331dbf9643aa45f1
miyado <10195648+hmiyado@users.noreply.github.com>
2023-03-11 16:26:45 +0900

    Use clientStorage to persistence slack webhook url

--- src/code.ts
+++ src/code.ts
@@ -1,25 +1,27 @@
-let slackUrl = ''
+const KeySlackWebhookUrl = 'slackWebhookUrl'
+
 figma.showUI(__uiFiles__.postToSlack, {
   visible: false
 })
 figma.on('run', ({ parameters }) => {
-  slackUrl = parameters?.['slackWebhookUrl']
+  const slackWehookUrl = parameters?.[KeySlackWebhookUrl]
+  console.log(slackWehookUrl)
+  figma.clientStorage.setAsync(KeySlackWebhookUrl, slackWehookUrl)
 })
 
-let count = 1
 figma.on('documentchange', (event) => {
-  count += 1
-  if (count > 5) {
-    return
-  }
-  for (const changes of event.documentChanges) {
-    console.log(changes)
-    figma.ui.postMessage({
-      type: 'onDocumentChange',
-      content: {
-        slackUrl,
-        changes
-      }
-    })
-  }
+  (async () => {
+    for (const changes of event.documentChanges) {
+      console.log(changes)
+      const slackWebhookUrl = await figma.clientStorage.getAsync(KeySlackWebhookUrl)
+      console.log(slackWebhookUrl)
+      figma.ui.postMessage({
+        type: 'onDocumentChange',
+        content: {
+          slackWebhookUrl,
+          changes
+        }
+      })
+    }
+  })()
 })
